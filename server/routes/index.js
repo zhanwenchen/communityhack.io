dbfa8562cbd952859e17514c3f2abaa64c749b22
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2017-02-11 17:33:16 -0500

    successfully added data to psql

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Create new user
 router.post('/users', function(req, res) {
   models.User.create({
     firstName: req.body.firstName,
@@ -15,5 +16,20 @@ router.post('/users', function(req, res) {
     res.json(user);
   });
 });
+
+// get all users
+router.get('/users', function(req, res) {
+  models.User.findAll({}).then(function(users) {
+    res.json(users);
+  });
+});
+
+// get all teams
+router.get('/teams', function(req, res) {
+  models.Team.findAll({}).then(function(teams) {
+    res.json(teams);
+  });
+});
+
 
 module.exports = router;
